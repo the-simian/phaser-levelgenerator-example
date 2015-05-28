d09356980d11a7cdc2fa62acb92eddc2306ff899
the-simian <harlinjesse@gmail.com>
2015-05-27 23:05:40 -0400

    updated states

--- src/states.js
+++ src/states.js
@@ -1,12 +1,14 @@
 
 
-var boot = require('./states/boot');
+var bootState = require('./states/boot');
+var gameState = require('./states/game');
+var menuState = require('./states/menu');
+var preloaderState = require('./states/preloader');
 
 
-
-
-
-
-game.state.add('boot', boot);
+game.state.add('boot', bootState);
+game.state.add('boot', gameState);
+game.state.add('boot', menuState);
+game.state.add('boot', preloaderState);
 
 
---
+++ src/states/preloader.js
@@ -0,0 +1,34 @@
+'use strict';
+var game = require('../game'),
+  Phaser = require('phaser').Phaser;
+
+function create() {
+
+}
+
+function update() {
+
+}
+
+function paused() {
+
+}
+
+function render() {
+
+}
+
+function shutdown() {
+
+}
+
+var preloaderState = {
+  create: create,
+  update: update,
+  paused: paused,
+  render: render,
+  shutdown: shutdown
+};
+
+
+module.exports = preloaderState;
